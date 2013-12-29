f290c6c927a829a0e328b4011029d08acb307d7c
Lior Shefer <lior@billguard.com>
2013-12-29 19:49:39 +0200

    changed webserver setup

--- server.js
+++ server.js
@@ -78,5 +78,7 @@ app.configure(function(){
     app.use(app.router);
 });
 
-app.listen(3000);
-console.log('Listening on port 3000');
+var port = process.env.PORT || 3000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
